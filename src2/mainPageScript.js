3cb7744bf80ea85f2552c85c39f1cf6ebe142792
Abdulmagid969 <aa1601514@qu.edu.qa>
2024-03-14 13:50:01 +0300

    commit

--- src2/mainPageScript.js
+++ src2/mainPageScript.js
@@ -7,11 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
         data.forEach(item => {
             const itemDiv=document.createElement("div");
                 itemDiv.classList.add("item");
+            
             const itemImgDiv=document.createElement("div");
+                itemImgDiv.classList.add('photo-div');
                 const photo=document.createElement("img");
                 photo.src=item.link;
-                
+                photo.alt="Item";
+                photo.classList.add("photo");
+                itemImgDiv.appendChild(photo);
+            itemDiv.appendChild(itemImgDiv);
+
+            const itemDescDiv=document.createElement("div");
+                const para =document.createElement("p");
+                para.textContent=item.description;
+                para.classList.add("disc");
+                itemDescDiv.appendChild(para);
+            itemDiv.appendChild(itemDescDiv);
             console.log(photo);
+            main.appendChild(itemDiv);
         });
 
         });
