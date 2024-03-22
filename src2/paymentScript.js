5cea4c7111a5d527a237c3f41831beda67b4b145
Abdulmagid969 <aa1601514@qu.edu.qa>
2024-03-22 04:20:39 +0300

    c

--- src2/paymentScript.js
+++ src2/paymentScript.js
@@ -1,9 +1,44 @@
+
 const cartItems=localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")): [];
-const tBody=document.querySelector("#cart-items");
+const user=localStorage.getItem("user")? JSON.parse(localStorage.getItem("user")): [];
+
+document.querySelector("#username").textContent=user[0] ? user[0].userName: "-";
+document.querySelector("#pay-button").addEventListener("click",payAction);
 console.log(cartItems)
 initialzeItems();
 
 function initialzeItems(){
-   
-    console.log(tBody);
+   const tBody=document.querySelector("#cart-items");
+
+   cartItems.forEach((i) =>{
+    const bodyRow=document.createElement("tr");
+    const itemC=document.createElement("td");
+    const priceC=document.createElement("td");
+    const quantityC=document.createElement("td");
+
+    itemC.textContent=i.item;
+    priceC.textContent=i.price;
+    quantityC.textContent=i.quantity;
+    bodyRow.appendChild(itemC);
+    bodyRow.appendChild(priceC);
+    bodyRow.appendChild(quantityC);
+    tBody.appendChild(bodyRow);
+    });
 }
+
+function payAction(){
+    const userBankAccount=user[0] ? user[0].bankAccount : -1;
+    const inputBankAccount=document.querySelector("#bankAccount").value;
+
+    if(inputBankAccount.length==0 ||userBankAccount==-1 || userBankAccount !==inputBankAccount){
+        document.querySelector("#bankError").classList.remove("hidden");
+        return;
+    };
+    redirectToThankyouPage();
+
+}; 
+
+function redirectToThankyouPage(){
+    window.location.href = "./thankyou.html";
+}
+
